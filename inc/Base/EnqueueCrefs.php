83bc52b86f06dbde8a936595194dc831e5139eac
Kimsea Sok <basicblogtalk@gmail.com>
2018-09-09 12:26:24 +0700

    Update security issues and code formating
    Add escape and sanitize data.

--- inc/Base/EnqueueCrefs.php
+++ inc/Base/EnqueueCrefs.php
@@ -16,25 +16,7 @@ class EnqueueCrefs extends ControlerCrefs
         add_action('admin_enqueue_scripts', array($this, 'admin_enqueue'));
         
 	}
-	
-    /*
-     * Helper method for generate custom message.
-     * 
-     * Param    String  $message    String message
-     * Return   String  $message    String message
-     */
-    public function getMessage($message) {
-        
-        $re = '/\{.*\}/';
-        $str = $message;
-        preg_match($re, $str, $matches, PREG_OFFSET_CAPTURE, 0);
-        $message = substr($matches[0][0], 0, -1);
-        $message = substr($message, 1);
-        
-        return $message;        
-    }
     
-    //
 	public function enqueue(){
         
         $post_type = (get_post_type()) ? get_post_type() : '';
@@ -44,18 +26,19 @@ class EnqueueCrefs extends ControlerCrefs
         $is_allowed = (is_array($allowed_post_types) && isset($allowed_post_types["{$post_type}"])) ? $allowed_post_types["{$post_type}"] : 'off';
         
         //return is CommenRefs isn't turned on or comment is not open
-        if(comments_open() === false || $is_allowed !== 'on') {
+        if(false === comments_open() || $is_allowed !== 'on') {
             
             return;
             
         }
         
         //get current user email base on login user id
-        $user = (get_current_user_id()) ? get_current_user_id() : '';
-        $user_email = (get_the_author_meta('email', $user)) ? get_the_author_meta('email', $user) : '';
+        $current_user = (get_current_user_id()) ? get_current_user_id() : '';
+        $current_user_email = (get_the_author_meta('email', $current_user)) ? get_the_author_meta('email', $current_user) : '';
         
         //get credit setting option
         $miscellaneous = (get_option('crefs_miscellaneous')) ? get_option('crefs_miscellaneous') : '';
+        
         $credited = (is_array($miscellaneous) && isset($miscellaneous['credit_CommentRefs']) && $miscellaneous['credit_CommentRefs'] == 'on') ? true : false;
         
         //setup api setting for localize script
@@ -63,12 +46,12 @@ class EnqueueCrefs extends ControlerCrefs
         $settings['title']      =   (get_the_title(get_the_ID())) ? get_the_title(get_the_ID()) : '';
         $settings['permalink']  =   (get_permalink(get_the_ID())) ? get_permalink(get_the_ID()) : '';
         $settings['wponce']     =   wp_create_nonce('CommentRefs');
-        $settings['website']    =   (get_the_author_meta('user_url', $user)) ? get_the_author_meta('user_url', $user) : '';
+        $settings['website']    =   (get_the_author_meta('user_url', $current_user)) ? get_the_author_meta('user_url', $current_user) : '';
         $settings['loggedin']   =   (is_user_logged_in()) ? true : false;
         $settings['is_admin']   =   (current_user_can('switch_themes')) ? true : false;
-        $settings['is_author']  =   ($user == get_post_field('post_author', get_the_ID())) ? true : false;
-        $settings['comments']   =   (get_comments(array('author_email' => $user_email, 'count' => true))) ? get_comments(array('author_email' => $user_email, 'count' => true)) : 0;
-        $settings['credit_message']     =   ($credited) ? __('CommentRef plugin by <a href="https://basicblogtalk.com" title="Basic Blog Talk" target="_blank">basicblogtalk.com</a>', 'CommentRefs') : '';
+        $settings['is_author']  =   ($current_user == get_post_field('post_author', get_the_ID())) ? true : false;
+        $settings['comments']   =   (get_comments(array('author_email' => $current_user_email, 'count' => true))) ? get_comments(array('author_email' => $current_user_email, 'count' => true)) : 0;
+        $settings['credit_message']     =   ($credited) ? _x('CommentRef plugin by <a href="https://basicblogtalk.com" title="Basic Blog Talk" target="_blank">basicblogtalk.com</a>', 'CommentRefs') : '';
         
         //get setting support for get 10 posts
         $get_ten_posts  = (get_option('crefs_get_ten_posts') && is_array(get_option('crefs_get_ten_posts'))) ? get_option('crefs_get_ten_posts') : '';
@@ -76,7 +59,11 @@ class EnqueueCrefs extends ControlerCrefs
         //Generate custom message for get 10 post
         if (is_array($get_ten_posts) && isset($get_ten_posts['message'])) {
             
-            $get_ten_posts['message'] = __($this->getMessage($get_ten_posts['message']) .' can enable ten post in the list', 'CommentRefs');
+            $get_ten_posts['message'] = __($get_ten_posts['message'] .' can enable ten post in the list', 'CommentRefs');
+            
+        } else {
+            
+            $get_ten_posts['message'] = '';
             
         }
         
@@ -85,7 +72,11 @@ class EnqueueCrefs extends ControlerCrefs
         
         if (is_array($get_dofollow) && isset($get_dofollow['message'])) {
             
-            $get_dofollow['message'] = __($this->getMessage($get_dofollow['message']) .' can get dofollow attribute', 'CommentRefs');
+            $get_dofollow['message'] = __($get_dofollow['message'] .' can get dofollow attribute', 'CommentRefs');
+            
+        } else {
+            
+            $get_dofollow['message'] = '';
             
         }
         
